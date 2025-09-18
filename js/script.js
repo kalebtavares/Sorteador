b7016abe16d457d290fad0db47446fc3ef2aefda
kalebtavares <kalebtavares@icloud.com>
2025-09-18 16:18:28 -0400

    Adicionar estilo e funcionalidade para exibir alertas ao sortear números

--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@ const min = Math.ceil (document.querySelector('.input-min').value)
 const max = Math.floor (document.querySelector('.input-max').value)
 
 const result = Math.floor(Math.random() * (max - min + 1)) + min;
-alert(result);
+mostrarAlerta(result);
 }
+
+function mostrarAlerta(result) {
+  const mensagem = `🎉 Número sorteado: ${result} 🎉`;
+  document.getElementById("mensagemAlerta").innerText = mensagem;
+  document.getElementById("alertaDivertido").style.display = "block";
+}
+
+function fecharAlerta() {
+  document.getElementById("alertaDivertido").style.display = "none";
+}
+
