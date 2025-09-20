0fea8725f93f63ea81545668d05907e3a0121717
kalebtavares <kalebtavares@icloud.com>
2025-09-20 08:52:18 -0400

    ajuste na comparação entre valor minímo e máximo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function generateNumber() {
   const min = Math.ceil(document.querySelector(".input-min").value);
   const max = Math.floor(document.querySelector(".input-max").value);
 
-  if (min >= max) {
+  if (max < min) {
     alert("O Valor  minimo deve ser MENOR que o valor máximo");
   } else {
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
